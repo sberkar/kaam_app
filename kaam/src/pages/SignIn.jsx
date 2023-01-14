d7f306fcaacddb2268f3c052c6670e8b95ad2213
SHUBHAM BERKAR <sberkar12@gmail.com>
2023-01-14 09:02:49 +0000

    "new project"

--- kaam/src/pages/SignIn.jsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "../contexts/auth-context";
-
-function SignIn() {
-    const { login } = useAuth()
-    return <button onClick={login}>Sign in with Google</button>
-}
-
-export default SignIn;
